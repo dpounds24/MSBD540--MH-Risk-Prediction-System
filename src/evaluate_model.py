814baacebea1811ef82543dc608086c263f40b32
dpounds24 <dpounds24@mmc.edu>
2024-08-05 05:38:15 -0500

    Update evaluate_model.py

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -1,1 +1,13 @@
+#Cross Validation
+from sklearn.model_selection import cross_val_predict
 
+y_pred = cross_val_predict(model, X_test, y_test, cv=5) 
+
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+# Metrics
+print("F1 score:", f1_score(y_test, y_pred, average='binary'))
+print("ROC AUC:", roc_auc_score(y_test, y_pred))
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Precision:", precision_score(y_test, y_pred))
+print("Recall:", recall_score(y_test, y_pred))
+print("Confusion Matrix:", confusion_matrix(y_test, y_pred))
