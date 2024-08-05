439ca84348ed5c1d4cff58213c80e01675ab8d95
dpounds24 <dpounds24@mmc.edu>
2024-08-05 05:35:14 -0500

    Update train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -1,1 +1,10 @@
+from sklearn.ensemble import RandomForestClassifier
 
+#Create Random Forest Classifier
+model = RandomForestClassifier() 
+
+#Fit Model
+model.fit(X_train,y_train)
+
+#Define predicted values
+y_pred=pd.DataFrame(model.predict_proba(X_test))[1].values
